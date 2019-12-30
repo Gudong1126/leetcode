a7b929c07f635677e462cb4955bd21380eb6adbe
zhangshuhao <zhang.shuhao@mwee.cn>
2019-12-30 09:56:29 +0800

    226, 232, 263

---
+++ 263.丑数.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=263 lang=javascript
+ *
+ * [263] 丑数
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isUgly = function(num) {
+    if (num <= 0) return false
+    if (num === 1) return true
+
+    const list = [2,3,5]
+
+    if (list.includes(num)) return true
+
+    for (const i of list) {
+        if (num % i === 0) return isUgly(Math.floor(num / i))
+    }
+
+    return false
+};
+// @lc code=end
+
