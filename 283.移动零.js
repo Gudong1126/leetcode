367971f59e88ce6442ea526743247806d37045ab
zhangshuhao <zhang.shuhao@mwee.cn>
2019-12-31 13:53:04 +0800

    283

---
+++ 283.移动零.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=javascript
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let index = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[index++] = nums[i]
+        }
+    }
+    for (let i = index; i < nums.length; i++) {
+        nums[index++] = 0
+        
+    }
+};
+// @lc code=end
+
